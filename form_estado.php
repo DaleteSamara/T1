f7e98a269fe6c0ccfef71041af2ffa62875e3e10
DaleteSamara <Daalete2@hotmail.com>
2018-04-14 11:18:49 -0300

    Formulario para os estados

--- form_estado.php
+++ form_estado.php
@@ -1,7 +1,28 @@
+<?php
+include ("cabecalho.php");
+?>
+
+	<style>
+	body{
+			margin: 100px auto;
+			text-align: center;
+			font-family: tahoma;
+			background-color: rgb(0,174,174);
+		}
+	div{
+		background-color: rgb(255,255,255);
+		margin: 2px auto;
+			border: 4px solid rgb(0,0,0);
+			padding: 25px;
+			font-size: 20px;
+			width: 700px;
+	}	
+
+	</style>
 <html>
-     <body>		
+     <body background="imagem.jpg">		
 		<form method="POST" action="cadastra_estado.php">
-		
+		<div>
 		 <label>Sigla</label>
 		 <input type="text" name="sigla"/>
 		
@@ -13,6 +34,7 @@
 		 <input type="submit" value="Enviar"/>	
 		
 		</form>
+		</div>
      </body>
 		
 </html>		
