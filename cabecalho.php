a7eaee537cc781b81a48a34c4ffc3f242542c384
DaleteSamara <Daalete2@hotmail.com>
2018-04-14 11:12:14 -0300

    Cabecalho

---
+++ cabecalho.php
@@ -0,0 +1,21 @@
+<style>
+ div{
+	background-color: rgb(255,255,255);
+		margin: 2px auto;
+			border: 4px solid rgb(0,0,0);
+ }
+</style>
+
+<head>
+	<title> Trabalho 1° bimestre </title>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" type="text/css" href = "estilos.css"/>
+</head>
+<body>
+<div>
+<a href="form_estado.php">Cadastrar Estado</a> |
+	<a href="form_cidade.php">Cadastrar Cidade</a>|
+	<a href="form_cadastro.php">Cadastrar Usuário</a>
+</div>	
+	
+</body>
